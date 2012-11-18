acebe74772bf2fb7de8bc218c675159a8bcfb4cf
Joe Wilson <joe@volaresystems.com>
2012-11-17 20:27:39 -0700

    initial home page working (mostly)

---
+++ PD/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SLC.MvcApplication" Language="C#" %>
---
+++ PD/Scripts/ExpandCollapse.js
@@ -0,0 +1,11 @@
+﻿$(function () {
+    var expander = $('.expand-collapse');
+    expander.filter('.collapsed').next().hide();
+    expander.filter('.expanded').next().show();
+    expander.click(function () {
+        $(this).toggleClass('expanded collapsed').next().slideToggle('fast', function () {           
+            // hack to fix IE rendering issue where padding isn't applied correctly to toggled items until focus is moved elsewhere
+            $('body').focus();
+        });
+    });
+});
